2cfd2274788321f0f7da6391058255f95a1af1fc
Antonio José Mota Ferreira Brandão <catce.2024111bemc0029@aluno.ifpi.edu.br>
2025-04-23 10:44:31 -0300

    Update tets.c

--- tets.c
+++ tets.c
@@ -18,8 +18,8 @@
 #define I2C_SCL 15
 #define ADC_Y 26
 #define ADC_X 27
-#define SSID "OP"
-#define PASS "onepiece"
+#define SSID "ENZOBOOK 4142"
+#define PASS "ENZOMELO10"
 #define ADC_MAX 4095
 #define HOST "api.thingspeak.com"
 #define PORT 443  // HTTPS
@@ -115,24 +115,34 @@ char *bussola(uint x, uint y){
     return result;
 
 }
-
+char newvalue[500];
 int main()
 {
+    srand(1);
+    int randnum = rand()%100;
+    
     data *direction = (data *)malloc(sizeof(data));
 
     setup();
 
+    snprintf(newvalue, sizeof(newvalue), "/update?api_key=B29KRC6XD1D1JGJ3&field1=%d", randnum);
+    printf("%s\n", newvalue);
+
+
     EXAMPLE_HTTP_REQUEST_T req1 = {0};
     req1.hostname = HOST;
-    req1.url = URL_REQUEST;
+    req1.url = newvalue;
     req1.headers_fn = http_client_header_print_fn;
     req1.recv_fn = http_client_receive_print_fn;
     int result = http_client_request_sync(cyw43_arch_async_context(), &req1);
     result += http_client_request_sync(cyw43_arch_async_context(), &req1);
-
+    printf("result: %d\n", result);
+    
     EXAMPLE_HTTP_REQUEST_T req2 = req1;
     result += http_client_request_async(cyw43_arch_async_context(), &req1);
     result += http_client_request_async(cyw43_arch_async_context(), &req2);
+    printf("result: %d\n", result);
+
     while(!req1.complete && !req2.complete){
         async_context_poll(cyw43_arch_async_context());
         async_context_wait_for_work_ms(cyw43_arch_async_context(), 1000);
@@ -143,17 +153,52 @@ int main()
     result += http_client_request_sync(cyw43_arch_async_context(), &req1);
     altcp_tls_free_config(req1.tls_config);
 
-    sleep_ms(15000);
+    memset(&req1, 0, sizeof req1);
+    memset(&req2, 0, sizeof req2);
 
-    if (result != 0) {
-        panic("test failed");
-    }
-
-    cyw43_arch_deinit();
+    printf("result: %d\n", result);
     
+    sleep_ms(60000);    
     while (true) {
+
+        randnum += rand()%100; 
+        
+        snprintf(newvalue, sizeof(newvalue), "/update?api_key=B29KRC6XD1D1JGJ3&field1=%d", randnum);
+
+        printf("%s\n", newvalue);
+        
         cyw43_arch_poll();  // Necessário para manter o Wi-Fi ativo
 
-        sleep_ms(15000);
+        req1.hostname = HOST;
+        req1.url = newvalue;
+        req1.headers_fn = http_client_header_print_fn;
+        req1.recv_fn = http_client_receive_print_fn;
+        result += http_client_request_async(cyw43_arch_async_context(), &req1);
+        result += http_client_request_async(cyw43_arch_async_context(), &req2);
+        printf("result: %d\n", result);
+
+        req2 = req1;
+        int result = http_client_request_sync(cyw43_arch_async_context(), &req1);
+        result += http_client_request_sync(cyw43_arch_async_context(), &req1);
+        printf("result: %d\n", result);
+    
+
+        while(!req1.complete && !req2.complete){
+            async_context_poll(cyw43_arch_async_context());
+            async_context_wait_for_work_ms(cyw43_arch_async_context(), 1000);
+        }
+
+        req1.tls_config = altcp_tls_create_config_client(NULL, 0);
+        result += http_client_request_sync(cyw43_arch_async_context(), &req1);
+        result += http_client_request_sync(cyw43_arch_async_context(), &req1);
+        altcp_tls_free_config(req1.tls_config);
+
+        memset(&req1, 0, sizeof req1);
+        memset(&req2, 0, sizeof req2);
+
+        sleep_ms(60000);
+
+        tight_loop_contents();
     }
+    return 0;
 }
